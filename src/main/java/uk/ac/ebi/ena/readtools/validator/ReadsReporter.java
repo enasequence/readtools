be4c13faa33c87f210db2616eb9d25b6bcbacdb2
Eugene Ivanov <eugene@ebi.ac.uk>
2019-09-11 09:49:11 +0100

    write() creates file if it doesn't exist, throws RuntimeException in case of failure

--- src/main/java/uk/ac/ebi/ena/readtools/validator/ReadsReporter.java
+++ src/main/java/uk/ac/ebi/ena/readtools/validator/ReadsReporter.java
@@ -2,34 +2,42 @@ package uk.ac.ebi.ena.readtools.validator;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 import uk.ac.ebi.ena.readtools.webin.cli.rawreads.ScannerMessage;
 import uk.ac.ebi.ena.readtools.webin.cli.rawreads.ScannerMessage.ScannerErrorMessage;
 
-public class ReadsReporter
-{
+public class ReadsReporter {
+
   public void write(File file, Severity severity, String origin, String message) {
-    try (PrintWriter writer = new PrintWriter(file)) {
+    try {
+      if (!file.exists()) {
+        file.createNewFile();
+      }
+      PrintWriter writer = new PrintWriter(file);
       String out = String.format("%s: %s%s", severity.toString(), origin, message);
       writer.write(out);
       writer.flush();
-    } catch (FileNotFoundException e) {
-      System.out.println(e);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
   }
 
   public void write(File file, ScannerMessage scannerMessage) {
-    Severity severity = scannerMessage instanceof ScannerErrorMessage ? Severity.ERROR : Severity.INFO;
+    Severity severity =
+        scannerMessage instanceof ScannerErrorMessage ? Severity.ERROR : Severity.INFO;
     String msg = scannerMessage.getMessage();
-    if (null != scannerMessage.getThrowable())
+    if (null != scannerMessage.getThrowable()) {
       msg += scannerMessage.getThrowable().toString();
+    }
     write(file, severity, scannerMessage.getOrigin(), msg);
   }
 
   public void write(File file, List<ScannerMessage> messages) {
-    for (ScannerMessage sm: messages)
+    for (ScannerMessage sm : messages) {
       write(file, sm);
+    }
   }
 
   public enum Severity {
