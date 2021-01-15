c692e5fdace0f22b6784037f6ecdc6a2d813e9ff
M. Haseeb <mhaseeb@ebi.ac.uk>
2021-01-15 16:24:04 +0000

    ENA-4123
    - added a small utility to check if bam files have reverse reads in them.
    - updated year text in the license text.

---
+++ src/main/java/uk/ac/ebi/ena/readtools/utils/SAMReverseReadCheck.java
@@ -0,0 +1,88 @@
+/*
+* Copyright 2010-2021 EMBL - European Bioinformatics Institute
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+* file except in compliance with the License. You may obtain a copy of the License at
+* http://www.apache.org/licenses/LICENSE-2.0
+* Unless required by applicable law or agreed to in writing, software distributed under the
+* License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+* CONDITIONS OF ANY KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations under the License.
+*/
+package uk.ac.ebi.ena.readtools.utils;
+
+import htsjdk.samtools.SAMRecord;
+import htsjdk.samtools.SamReader;
+import htsjdk.samtools.SamReaderFactory;
+import htsjdk.samtools.ValidationStringency;
+import uk.ac.ebi.ena.readtools.sam.Sam2Fastq;
+
+import java.io.File;
+
+public class SAMReverseReadCheck {
+
+    private final File samFile;
+
+    public static void main(String[] args) {
+        if (args.length == 0 || args[0].isEmpty()) {
+            System.out.println("Provide path to a SAM/BAM/CRAM file.");
+            System.exit(1);
+        }
+
+        File samFile = new File(args[0]);
+        if (!samFile.exists() || samFile.length() == 0) {
+            System.out.println("Input SAM/BAM/CRAM file does not exists or is empty.");
+            System.exit(1);
+        }
+
+        Output output = new SAMReverseReadCheck(samFile).check();
+        if (output == null) {
+            System.out.println("No reverse reads found.");
+        } else {
+            System.out.println(String.format("%s\n%s\n%s\n%d",
+                    output.readName, output.bases, output.qualities, output.flags));
+        }
+    }
+
+    public SAMReverseReadCheck(File samFile) {
+        this.samFile = samFile;
+    }
+
+    public Output check() {
+        Output result = null;
+
+        SamReaderFactory.setDefaultValidationStringency(ValidationStringency.LENIENT);
+
+        try(SamReader samReader = SamReaderFactory.makeDefault().open(samFile)) {
+            for (final SAMRecord currentRecord : samReader) {
+
+                if (currentRecord.isSecondaryOrSupplementary() && !Sam2Fastq.INCLUDE_NON_PRIMARY_ALIGNMENTS) {
+                    continue;
+                }
+
+                // Skip non-PF reads as necessary
+                if (currentRecord.getReadFailsVendorQualityCheckFlag() && !Sam2Fastq.INCLUDE_NON_PF_READS) {
+                    continue;
+                }
+
+                if (currentRecord.getReadNegativeStrandFlag()) {
+                    result = new Output();
+                    result.flags = currentRecord.getFlags();
+                    result.readName = currentRecord.getReadName();
+                    result.bases = currentRecord.getReadString();
+                    result.qualities = currentRecord.getBaseQualityString();
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+
+        return result;
+    }
+
+    class Output {
+
+        public int flags;
+        public String readName, bases, qualities;
+    }
+}
---
+++ src/test/java/uk/ac/ebi/ena/readtools/utils/SAMReverseReadCheckTest.java
@@ -0,0 +1,42 @@
+/*
+* Copyright 2010-2021 EMBL - European Bioinformatics Institute
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+* file except in compliance with the License. You may obtain a copy of the License at
+* http://www.apache.org/licenses/LICENSE-2.0
+* Unless required by applicable law or agreed to in writing, software distributed under the
+* License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+* CONDITIONS OF ANY KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations under the License.
+*/
+package uk.ac.ebi.ena.readtools.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+import uk.ac.ebi.ena.readtools.sam.Sam2FastqTest;
+
+import java.io.File;
+import java.net.URL;
+
+public class SAMReverseReadCheckTest {
+
+    @Test
+    public void reverseReadPresent() {
+        URL url = Sam2FastqTest.class.getClassLoader().getResource("sam2fastq/reverse-read.sam");
+        File file = new File( url.getFile() );
+
+        SAMReverseReadCheck.Output out = new SAMReverseReadCheck(file).check();
+
+        Assert.assertEquals("rid-0001", out.readName);
+        Assert.assertEquals("TCGATCGATCGATCGA", out.bases);
+        Assert.assertEquals("PONMLKJIHGFEDCBA", out.qualities);
+        Assert.assertEquals(157, out.flags);
+    }
+
+    @Test
+    public void reverseReadNotPresent() {
+        URL url = Sam2FastqTest.class.getClassLoader().getResource("bam2fastq/2fastq/8855_124.bam");
+        File file = new File( url.getFile() );
+
+        Assert.assertNull(new SAMReverseReadCheck(file).check());
+    }
+}
